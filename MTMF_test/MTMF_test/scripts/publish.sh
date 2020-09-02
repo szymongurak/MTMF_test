5468d5236548e19d783c6ccc6ad7868872dfda22
g_szymon <gurakszymon@gmail.com>
2020-09-02 22:04:08 +0200

    newTravisCommit

--- MTMF_test/MTMF_test/scripts/publish.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dotnet publish ./src/Passenger.Api -c Release -o ./bin/Docker
--- MTMF_test/MTMF_test/scripts/travis-success.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Executing after success scripts on branch $TRAVIS_BRANCH
-echo Publishing application
-./scripts/publish.sh
-echo Building and pushing Docker images
-./scripts/docker-publish.sh
